7cfe11e9970f83487a22f9a551ff2c08852b2279
lesliebinbin <lesliebinbin19900129@gmail.com>
2020-08-22 11:49:02 +1000

    for c program

---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(int argc, char** argv){
+  printf("Hello, fuck this world!\n");
+  return 0;
+}
--- test/testing.cpp
+++
@@ -1,5 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
-TEST_CASE("cheers for the fucking test") {
-}
