f49f95d2e52b6ff252a691075ec7512a598698ba
lesliebinbin <lesliebinbin19900129@gmail.com>
2020-10-01 21:54:10 +1000

    adding useful headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,9 @@
+#include <algorithm>
+#include <fmt/ranges.h>
+#include <functional>
+#include <iostream>
+#include <range/v3/all.hpp>
+#include <string>
+#include <utility>
+#include <vector>
 int main(int argc, char *argv[]) { return 0; }
