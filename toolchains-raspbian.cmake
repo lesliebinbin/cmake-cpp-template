0032f5fc2040d11531b01e32588eda98e6141e50
Leslie Binbin <lesliebinbin19900129@gmail.com>
2020-12-17 15:19:37 +1000

    hahahhahahahha

---
+++ toolchains-raspbian.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(triple arm-linux-gnueabihf)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-10)
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_BUILD_TYPE debug)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
